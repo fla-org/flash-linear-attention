57fd584a18bdf168dd045e13add87cb2e361b810
yibozhong <yibozhong657@gmail.com>
2025-01-17 19:43:00 +0800

    update __init__.py for vision models

--- fla/vision_models/__init__.py
+++ fla/vision_models/__init__.py
@@ -1,10 +1,43 @@
+from fla.vision_models.abc import ABCVisionConfig, ABCForImageClassification
+from fla.vision_models.bitnet import BitNetVisionConfig, BitNetForImageClassification
 from fla.vision_models.delta_net import DeltaNetVisionConfig, DeltaNetForImageClassification
+from fla.vision_models.gated_deltanet import GatedDeltaNetVisionConfig, GatedDeltaNetForImageClassification
+from fla.vision_models.gla import GLAVisionConfig, GLAForImageClassification
+from fla.vision_models.gsa import GSAVisionConfig, GSAForImageClassification
+from fla.vision_models.hgrn import HGRNVisionConfig, HGRNForImageClassification
+from fla.vision_models.hgrn2 import HGRN2VisionConfig, HGRN2ForImageClassification
+from fla.vision_models.linear_attn import LinearAttentionVisionConfig, LinearAttentionForImageClassification
+from fla.vision_models.retnet import RetNetVisionConfig, RetNetForImageClassification
+from fla.vision_models.rwkv6 import RWKV6VisionConfig, RWKV6ForImageClassification
+from fla.vision_models.transformer import TransformerVisionConfig, TransformerForImageClassification
 from fla.vision_models.utils import ImageEmbeddings, PatchEmbeddings, Pooler
 
 __all__ = [
+    'ABCVisionConfig',
+    'ABCForImageClassification',
+    'BitNetVisionConfig',
+    'BitNetForImageClassification',
     'DeltaNetVisionConfig',
     'DeltaNetForImageClassification',
+    'GatedDeltaNetVisionConfig',
+    'GatedDeltaNetForImageClassification',
+    'GLAVisionConfig',
+    'GLAForImageClassification',
+    'GSAVisionConfig',
+    'GSAForImageClassification',
+    'HGRNVisionConfig',
+    'HGRNForImageClassification',
+    'HGRN2VisionConfig',
+    'HGRN2ForImageClassification',
+    'LinearAttentionVisionConfig',
+    'LinearAttentionForImageClassification',
+    'RetNetVisionConfig',
+    'RetNetForImageClassification',
+    'RWKV6VisionConfig',
+    'RWKV6ForImageClassification',
+    'TransformerVisionConfig',
+    'TransformerForImageClassification',
     'ImageEmbeddings',
     'PatchEmbeddings',
-    'Pooler'
+    'Pooler',
 ]
