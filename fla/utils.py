8e2ce2a9637349c6644abe38b1eb30a935e48c2c
Zhiyuan Li <uniartisan2017@gmail.com>
2025-04-01 11:44:38 +0000

    use `tl.gather` in `chunk_dplr_bwd_kernel_intra`

--- fla/utils.py
+++ fla/utils.py
@@ -167,6 +167,13 @@ use_cuda_graph = (is_nvidia and os.environ.get('FLA_USE_CUDA_GRAPH', '0') == '1'
 
 # Nvidia Ampere or newer, haven't check AMD and intel yet.
 is_tf32_supported = (is_nvidia and torch.cuda.get_device_capability(0)[0] >= 8)
+triton_have_gather = hasattr(triton, 'gather')
+
+
+if not triton_have_gather:
+    def gather(*args, **kwargs):
+        pass
+    triton.language.gather = gather
 
 
 def get_all_max_shared_mem():
