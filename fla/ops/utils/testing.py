6da26a8cb3ce5fd3601891de295d6ba9d59f8568
Zhiyuan Li <uniartisan2017@gmail.com>
2025-04-04 05:01:53 +0000

    update testing.py

--- fla/ops/utils/testing.py
+++ fla/ops/utils/testing.py
@@ -9,18 +9,21 @@ def get_abs_err(x, y):
 
 
 def get_err_ratio(x, y):
-    err = (x-y).flatten().square().mean().sqrt().item()
+    err = (x.detach()-y.detach()).flatten().square().mean().sqrt().item()
     base = (x).flatten().square().mean().sqrt().item()
     return err / (base + 1e-15)
 
 
 def assert_close(prefix, ref, tri, ratio, warning=False):
-    msg = f"{prefix} diff: {get_abs_err(ref, tri):.6f} ratio: {get_err_ratio(ref, tri):.6f}"
+    abs_err = get_abs_err(ref, tri)
+    msg = f"{prefix} diff: {abs_err:.6f} ratio: {get_err_ratio(ref, tri):.6f}"
     print(msg)
     error_rate = get_err_ratio(ref, tri)
-    if warning or str(prefix).strip().lower() == "dh0" or (FLA_CI_ENV and error_rate < 0.01):
-        if error_rate > ratio:
+    if warning or str(prefix).strip().lower() == "dh0" or FLA_CI_ENV:
+        if (error_rate > ratio) and (error_rate < 0.01 or abs_err < 0.3):
             import warnings
             warnings.warn(msg)
+        else:
+            assert error_rate < ratio, msg
     else:
         assert error_rate < ratio, msg
