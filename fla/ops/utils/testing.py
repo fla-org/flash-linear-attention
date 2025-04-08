136273132f0875f7c653ee1d475fb4a96c41be03
Zhiyuan Li <uniartisan2017@gmail.com>
2025-04-08 09:15:09 +0000

    [Test] Fix numerical stability in error ratio calculation

--- fla/ops/utils/testing.py
+++ fla/ops/utils/testing.py
@@ -11,16 +11,18 @@ def get_abs_err(x, y):
 def get_err_ratio(x, y):
     err = (x.detach()-y.detach()).flatten().square().mean().sqrt().item()
     base = (x).flatten().square().mean().sqrt().item()
-    return err / (base + 1e-15)
+    return err / (base + 1e-8)
 
 
-def assert_close(prefix, ref, tri, ratio, warning=False):
-    abs_err = get_abs_err(ref, tri)
-    msg = f"{prefix} diff: {abs_err:.6f} ratio: {get_err_ratio(ref, tri):.6f}, tollerance: {ratio:.6f}"
+def assert_close(prefix, ref, tri, ratio, warning=False, err_atol=1e-6):
+    abs_atol = get_abs_err(ref, tri)
+    msg = f"{prefix} diff: {abs_atol:.6f} ratio: {get_err_ratio(ref, tri):.6f}"
     print(msg)
     error_rate = get_err_ratio(ref, tri)
+    if abs_atol <= err_atol:
+        return
     if warning or (FLA_CI_ENV and error_rate < 0.01):
-        if (error_rate > ratio) and (error_rate < 0.01 or abs_err < 0.3):
+        if error_rate > ratio:
             import warnings
             warnings.warn(msg)
         else:
