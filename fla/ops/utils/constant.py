ac051ec4bbabeb569dedc29acf8d3da120100a81
Zhiyuan Li <uniartisan2017@gmail.com>
2025-04-17 12:38:26 +0000

    Update constant in fp32

--- fla/ops/utils/constant.py
+++ fla/ops/utils/constant.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2023-2025, Songlin Yang, Yu Zhang
 
-INV_LOG2 = 1.44269504
+# Approximate value of 1/ln(2), used for log/exp base conversion
+# Best FP32 approximation: 1.4426950216 (hex 0x3FB8AA3B)
+INV_LOG2 = 1.4426950216
