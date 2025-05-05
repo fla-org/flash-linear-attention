fd291404a0e86b59ed5a0b919f0b3c6bf510ae76
Zhiyuan Li <uniartisan2017@gmail.com>
2025-05-05 08:23:56 +0000

    [DPLR] remove fp32 error

--- fla/ops/generalized_delta_rule/dplr/chunk.py
+++ fla/ops/generalized_delta_rule/dplr/chunk.py
@@ -331,7 +331,7 @@ def chunk_dplr_delta_rule(
             "Please verify your input tensor format matches the expected shape [B, T, H, ...]."
         )
     if q.dtype == torch.float32:
-        raise DeprecationWarning(
+        warnings.warn(
             """ChunkDeltaRuleFunction does not support float32. Please use bfloat16.
             If you want to use float32, please solve the issue by yourself."""
         )
