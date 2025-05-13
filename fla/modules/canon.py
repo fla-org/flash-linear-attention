b7292f5f1d702859107b5f82fd9f4fb3830ba8ba
Yu Zhang <yzhang.cs@outlook.com>
2025-05-13 21:50:20 +0000

    Support bwd process

--- fla/modules/canon.py
+++ fla/modules/canon.py
@@ -29,7 +29,7 @@ from fla.utils import get_multiprocessor_count, input_guard
     key=['B', 'D', 'W', 'NB'],
 )
 @triton.jit
-def canon_fwd_kernel(
+def causal_conv1d_fwd_kernel(
     x,
     y,
     weight,
@@ -87,7 +87,101 @@ def canon_fwd_kernel(
     tl.store(p_y, tl.cast(b_y, dtype=p_y.dtype.element_ty, fp_downcast_rounding='rtne'), boundary_check=(0, 1))
 
 
-def canon_fwd(
+@triton.heuristics({
+    'HAS_WEIGHT': lambda args: args['weight'] is not None,
+    'HAS_BIAS': lambda args: args['bias'] is not None,
+    'HAS_RESIDUAL': lambda args: args['residual'] is not None,
+    'IS_VARLEN': lambda args: args['cu_seqlens'] is not None
+})
+@triton.autotune(
+    configs=[
+        triton.Config({'BD': BD}, num_warps=num_warps, num_stages=num_stages)
+        for BD in [128, 256]
+        for num_warps in [4, 8, 16, 32]
+        for num_stages in [2, 3, 4, 5]
+    ],
+    key=['B', 'D', 'W', 'NB'],
+)
+@triton.jit
+def causal_conv1d_bwd_kernel(
+    x,
+    weight,
+    bias,
+    residual,
+    dy,
+    dx,
+    dw,
+    db,
+    dr,
+    cu_seqlens,
+    chunk_indices,
+    T,
+    B: tl.constexpr,
+    D: tl.constexpr,
+    W: tl.constexpr,
+    BT: tl.constexpr,
+    BD: tl.constexpr,
+    NB: tl.constexpr,
+    ACTIVATION: tl.constexpr,
+    HAS_WEIGHT: tl.constexpr,
+    HAS_BIAS: tl.constexpr,
+    HAS_RESIDUAL: tl.constexpr,
+    IS_VARLEN: tl.constexpr,
+):
+    i_d, i_t, i_b = tl.program_id(0), tl.program_id(1), tl.program_id(2)
+    if IS_VARLEN:
+        i_n, i_t = tl.load(chunk_indices + i_t * 2).to(tl.int32), tl.load(chunk_indices + i_t * 2 + 1).to(tl.int32)
+        bos, eos = tl.load(cu_seqlens + i_n), tl.load(cu_seqlens + i_n + 1)
+        T = eos - bos
+    else:
+        i_n = i_b
+        bos, eos = i_b * T, i_b * T + T
+    i_bt = i_b * tl.num_programs(1) + i_t
+
+    o_d = i_d * BD + tl.arange(0, BD)
+    m_d = o_d < D
+
+    if HAS_WEIGHT:
+        p_x = tl.make_block_ptr(x + bos * D, (T, D), (D, 1), (i_t * BT, i_d * BD), (BT, BD), (1, 0))
+        b_x = tl.load(p_x, boundary_check=(0, 1))
+
+    b_dx = tl.zeros((BT, BD), dtype=tl.float32)
+    if HAS_BIAS:
+        b_db = tl.zeros((BD,), dtype=tl.float32)
+    for i_w in range(0, W):
+        p_dy = tl.make_block_ptr(dy + bos * D, (T, D), (D, 1), (i_t * BT + i_w, i_d * BD), (BT, BD), (1, 0))
+
+        # [BT, BD]
+        b_dy = tl.load(p_dy, boundary_check=(0, 1))
+
+        if HAS_WEIGHT:
+            o_w = W - i_w - 1
+            # [BT, BD]
+            b_wdy = b_dy * tl.load(weight + o_d * W + o_w, mask=m_d, other=0)
+            # [BD]
+            b_dw = tl.sum(b_dy * b_x, 0)
+            b_dw = tl.cast(b_dw, dtype=dw.dtype.element_ty, fp_downcast_rounding='rtne')
+            tl.store(dw + i_bt * D*W + o_d * W + o_w, b_dw, mask=m_d)
+        if HAS_BIAS and i_w == 0:
+            b_db += tl.sum(b_dy, 0)
+        b_dx += b_wdy
+    if HAS_BIAS:
+        b_db = tl.cast(b_db, dtype=db.dtype.element_ty, fp_downcast_rounding='rtne')
+        tl.store(db + i_bt * D + o_d, b_db, mask=m_d)
+
+    if ACTIVATION == 'swish' or ACTIVATION == 'silu':
+        b_dx = b_dx * tl.sigmoid(b_dx)
+
+    if HAS_RESIDUAL:
+        p_residual = tl.make_block_ptr(residual + bos * D, (T, D), (D, 1), (i_t * BT, i_d * BD), (BT, BD), (1, 0))
+        b_residual = tl.load(p_residual, boundary_check=(0, 1))
+        b_dx += b_residual
+
+    p_dx = tl.make_block_ptr(dx + bos * D, (T, D), (D, 1), (i_t * BT, i_d * BD), (BT, BD), (1, 0))
+    tl.store(p_dx, tl.cast(b_dx, dtype=p_dx.dtype.element_ty, fp_downcast_rounding='rtne'), boundary_check=(0, 1))
+
+
+def causal_conv1d_fwd(
     x: torch.Tensor,
     weight: torch.Tensor,
     bias: torch.Tensor,
@@ -106,7 +200,7 @@ def canon_fwd(
 
     y = torch.empty_like(x)
     def grid(meta): return (triton.cdiv(D, meta['BD']), NT, B)
-    canon_fwd_kernel[grid](
+    causal_conv1d_fwd_kernel[grid](
         x=x,
         y=y,
         weight=weight,
@@ -125,7 +219,58 @@ def canon_fwd(
     return y.view(shape)
 
 
-class CanonFunction(torch.autograd.Function):
+def causal_conv1d_bwd(
+    x: torch.Tensor,
+    weight: torch.Tensor,
+    bias: torch.Tensor,
+    residual: torch.Tensor,
+    dy: torch.Tensor,
+    activation: Optional[str] = None,
+    cu_seqlens: Optional[torch.Tensor] = None
+):
+    shape = x.shape
+    if x.shape[-1] != weight.shape[0]:
+        x = rearrange(x, 'b t ... -> b t (...)')
+    B, T, D, W = *x.shape, weight.shape[1]
+    BT = min(64, triton.next_power_of_2(triton.cdiv(T, get_multiprocessor_count(x.device.index))))
+    chunk_indices = prepare_chunk_indices(cu_seqlens, BT) if cu_seqlens is not None else None
+    NT = len(chunk_indices) if cu_seqlens is not None else triton.cdiv(T, BT)
+    NB = triton.cdiv(T, 1024)
+
+    dx = torch.empty_like(x)
+    dw = weight.new_empty(B*NT, *weight.shape) if weight is not None else None
+    db = bias.new_empty(B*NT, *bias.shape) if bias is not None else None
+    dr = torch.empty_like(residual) if residual is not None else None
+    def grid(meta): return (triton.cdiv(D, meta['BD']), NT, B)
+    causal_conv1d_bwd_kernel[grid](
+        x=x,
+        weight=weight,
+        bias=bias,
+        residual=residual,
+        dy=dy,
+        dx=dx,
+        dw=dw,
+        db=db,
+        dr=dr,
+        cu_seqlens=cu_seqlens,
+        chunk_indices=chunk_indices,
+        B=B,
+        T=T,
+        D=D,
+        W=W,
+        BT=BT,
+        NB=NB,
+        ACTIVATION=activation,
+    )
+    if weight is not None:
+        dw = dw.sum(0).to(weight)
+    if bias is not None:
+        db = db.sum(0).to(bias)
+
+    return dx.view(shape), dw, db, dr
+
+
+class CausalConv1dFunction(torch.autograd.Function):
 
     @staticmethod
     @input_guard
@@ -141,15 +286,25 @@ class CanonFunction(torch.autograd.Function):
         ctx.activation = activation
         ctx.cu_seqlens = cu_seqlens
         ctx.save_for_backward(x, weight, bias, residual)
-        return canon_fwd(x, weight, bias, residual, activation, cu_seqlens)
+        return causal_conv1d_fwd(x, weight, bias, residual, activation, cu_seqlens)
 
     @staticmethod
     @input_guard
     def backward(ctx, dy: torch.Tensor):
-        raise NotImplementedError("Backward pass is not implemented for CanonFunction")
+        x, weight, bias, residual = ctx.saved_tensors
+        dx, dw, db, dr = causal_conv1d_bwd(
+            x=x,
+            weight=weight,
+            bias=bias,
+            dy=dy,
+            residual=residual,
+            activation=ctx.activation,
+            cu_seqlens=ctx.cu_seqlens
+        )
+        return dx, dw, db, dr, None, None
 
 
-def canon(
+def causal_conv1d(
     x: torch.Tensor,
     weight: Optional[torch.Tensor] = None,
     bias: Optional[torch.Tensor] = None,
@@ -158,8 +313,6 @@ def canon(
     cu_seqlens: Optional[torch.Tensor] = None
 ):
     """
-    Implementation of the Canon operation as mentioned in https://papers.ssrn.com/sol3/papers.cfm?abstract_id=5240330
-
     Args:
         x:
             Input tensor of shape [B, T, D].
@@ -176,14 +329,14 @@ def canon(
             Cumulative sequence lengths (optional)
 
     Returns:
-        Tensor of same shape as input with CanonFunction applied
+        Tensor of same shape as input with CausalConv1dFunction applied
     """
-    return CanonFunction.apply(x, weight, bias, residual, activation, cu_seqlens)
+    return CausalConv1dFunction.apply(x, weight, bias, residual, activation, cu_seqlens)
 
 
-class Canon(nn.Conv1d):
+class CausalConv1d(nn.Conv1d):
     """
-    Implementation of Canon layer as mentioned in https://papers.ssrn.com/sol3/papers.cfm?abstract_id=5240330
+    Implementation of CausalConv1d layer as mentioned in https://papers.ssrn.com/sol3/papers.cfm?abstract_id=5240330
     """
 
     def __init__(
@@ -273,8 +426,8 @@ class Canon(nn.Conv1d):
             x = x.mul_(mask.unsqueeze(-1))
         if output_final_state and cache is None:
             cache = x.new_zeros(N, D, W)
-
-        return canon(x, rearrange(self.weight, "d 1 w -> d w"), self.bias, residual, self.activation, cu_seqlens), cache
+        y = causal_conv1d(x, rearrange(self.weight, "d 1 w -> d w"), self.bias, residual, self.activation, cu_seqlens)
+        return y, cache
 
     @property
     def state_size(self) -> int:
