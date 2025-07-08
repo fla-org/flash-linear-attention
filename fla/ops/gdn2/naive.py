5e8e1df466ac61fdc63a4edd0fcfd88614c67247
Yu Zhang <yzhang.cs@outlook.com>
2025-07-08 09:39:25 +0000

    [GDN2] Add naive chunk impls

---
+++ fla/ops/gdn2/naive.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+
+from typing import Optional
+
+import torch
+from einops import rearrange
+
+
+def naive_chunk_gdn2(
+    q: torch.Tensor,
+    k: torch.Tensor,
+    v: torch.Tensor,
+    g: torch.Tensor,
+    beta: torch.Tensor,
+    scale: Optional[float] = None,
+    initial_state: Optional[torch.Tensor] = None,
+    output_final_state: bool = False,
+    chunk_size: int = 64
+):
+    dtype = v.dtype
+    B, T, H, K, V = *q.shape, v.shape[-1]
+    BT = chunk_size
+    NT = T // BT
+    if scale is None:
+        scale = K ** -0.5
+    assert T % BT == 0
+
+    q, k, v, g, beta = map(lambda x: rearrange(x, 'b (n c) h ... -> b h n c ...', c=BT).to(torch.float), [q, k, v, g, beta])
+    q = q * scale
+
+    # note that diagonal is masked.
+    mask = torch.triu(torch.ones(BT, BT, dtype=torch.bool, device=q.device), diagonal=0)
+    g = g.cumsum(-2)
+
+    A = torch.zeros(*q.shape[:-1], BT, dtype=torch.float, device=q.device)
+    for i in range(BT):
+        k_i = k[..., i, :]
+        g_i = g[..., i:i+1, :]
+        A[..., i] = torch.einsum('... c d, ... d -> ... c', k * (g - g_i).exp(), k_i)
+    A = A * beta[..., None]
+
+    A = -A.masked_fill(mask, 0)
+    for i in range(1, BT):
+        A[..., i, :i] = A[..., i, :i].clone() + (A[..., i, :, None].clone() * A[..., :, :i].clone()).sum(-2)
+    A = (A + torch.eye(BT, dtype=torch.float, device=q.device)) * beta[..., None, :]
+
+    w = A @ (g.exp() * k)
+    u = A @ v
+
+    S = k.new_zeros(B, H, K, V).to(q)
+    if initial_state is not None:
+        S += initial_state
+    o = torch.zeros_like(v)
+    mask = torch.triu(torch.ones(BT, BT, dtype=torch.bool, device=q.device), diagonal=1)
+    for i in range(0, NT):
+        # [B, H, BT, ...]
+        q_i, k_i, u_i, g_i, w_i = q[:, :, i], k[:, :, i], u[:, :, i], g[:, :, i], w[:, :, i]
+        A = torch.zeros(B, H, BT, BT, dtype=torch.float, device=q.device)
+        for j in range(BT):
+            k_j = k[:, :, i, j]
+            g_j = g[:, :, i, j:j+1, :]
+            A[..., j] = torch.einsum('... c d, ... d -> ... c', q_i * (g_i - g_j).exp(), k_j)
+        A = A.masked_fill(mask, 0)
+        v_i = u_i - w_i @ S
+        o[:, :, i] = (q_i * g_i.exp()) @ S + A @ v_i
+        S = S * g_i[:, :, -1:].exp()
+        # print(i, S.max())
+        S += ((g_i[:, :, -1:] - g_i).exp() * k_i).transpose(-1, -2) @ v_i
+    if not output_final_state:
+        S = None
+    return rearrange(o, 'b h n c d -> b (n c) h d').to(dtype), S
