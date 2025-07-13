2b51b28282799fd7f7f0dce4d2f36c4e766abfb1
Yu Zhang <yzhang.cs@outlook.com>
2025-07-13 13:44:00 +0000

    Fix isort

--- fla/models/__init__.py
+++ fla/models/__init__.py
@@ -20,8 +20,8 @@ from fla.models.linear_attn import LinearAttentionConfig, LinearAttentionForCaus
 from fla.models.mamba import MambaConfig, MambaForCausalLM, MambaModel
 from fla.models.mamba2 import Mamba2Config, Mamba2ForCausalLM, Mamba2Model
 from fla.models.mesa_net import MesaNetConfig, MesaNetForCausalLM, MesaNetModel
-from fla.models.mom import MomConfig, MomForCausalLM, MomModel
 from fla.models.mla import MLAConfig, MLAForCausalLM, MLAModel
+from fla.models.mom import MomConfig, MomForCausalLM, MomModel
 from fla.models.nsa import NSAConfig, NSAForCausalLM, NSAModel
 from fla.models.path_attn import PaTHAttentionConfig, PaTHAttentionForCausalLM, PaTHAttentionModel
 from fla.models.retnet import RetNetConfig, RetNetForCausalLM, RetNetModel
