319062911fc9d9329ee7c274ff049dcda101dc02
richardodliu <richardodliu@gmail.com>
2025-08-20 14:38:06 +0800

    Revert "add layer_classes=transformers.cache_utils.DynamicLayer"
    This reverts commit 5ddede776f4794d366c295c644fe4d37a0da9b68.

--- fla/models/utils.py
+++ fla/models/utils.py
@@ -21,7 +21,7 @@ class Cache(transformers.cache_utils.Cache):
         self,
         seen_tokens: int = 0
     ) -> Cache:
-        super().__init__(layer_classes=transformers.cache_utils.DynamicLayer)
+        super().__init__()
 
         self.states: List[Dict[str, Any]] = []
 
