fb5892b6b68300845ce18f2d4bc7f011d28aae61
yzhangcs <yzhang.cs@outlook.com>
2025-12-18 16:01:49 +0000

    reserv combined chunk

--- fla/ops/kda/__init__.py
+++ fla/ops/kda/__init__.py
@@ -1,9 +1,7 @@
 from .chunk import chunk_kda
-from .combined_chunk import combined_chunk_kda
 from .fused_recurrent import fused_recurrent_kda
 
 __all__ = [
     "chunk_kda",
-    "combined_chunk_kda",
     "fused_recurrent_kda",
 ]
