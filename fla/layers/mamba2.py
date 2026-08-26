ddf883d3aec0a6b94740a42f48895dcaa81d0390
taking-lying-flat <1615405@qq.com>
2026-08-26 21:03:50 +0800

    [Fix] Repair LogLinearMamba2 Triton cached decoding

--- fla/layers/mamba2.py
+++ fla/layers/mamba2.py
@@ -43,7 +43,7 @@ def apply_mask_to_padding_states(hidden_states, attention_mask):
     """
     Tunes out the hidden states for padding tokens, see https://github.com/state-spaces/mamba/issues/66
     """
-    if attention_mask is not None and attention_mask.shape[1] > 1 and attention_mask.shape[0] > 1:
+    if attention_mask is not None and attention_mask.shape == hidden_states.shape[:2]:
         dtype = hidden_states.dtype
         hidden_states = (hidden_states * attention_mask[:, :, None]).to(dtype)
 
