01a878d7ffce937efec9532ca21c395f67fb8833
yurekami <yurekami@users.noreply.github.com>
2026-08-28 18:26:02 +0800

    [Fix] Use NUM_WARPS_AUTOTUNE in causal_conv1d_bwd_kernel autotune

--- fla/modules/conv/triton/kernels.py
+++ fla/modules/conv/triton/kernels.py
@@ -144,7 +144,7 @@ def causal_conv1d_fwd_kernel(
     configs=[
         triton.Config({'BD': BD}, num_warps=num_warps)
         for BD in [16, 32, 64, 128]
-        for num_warps in [4, 8, 16, 32]
+        for num_warps in NUM_WARPS_AUTOTUNE
     ],
     key=['D', 'W', 'NB'],
     **autotune_cache_kwargs,
