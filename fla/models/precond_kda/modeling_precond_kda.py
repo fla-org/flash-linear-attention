72a58762cf46eef51927c146c40c7d5137116c12
taking-lying-flat <1615405@qq.com>
2026-09-03 20:30:15 +0800

    [Fix] Correct packed loss and cache defaults

--- fla/models/precond_kda/modeling_precond_kda.py
+++ fla/models/precond_kda/modeling_precond_kda.py
@@ -21,7 +21,7 @@ from transformers.utils.deprecation import deprecate_kwarg
 from fla.layers.attn import Attention
 from fla.layers.precond_kda import PrecondKDA
 from fla.models.precond_kda.configuration_precond_kda import PrecondKDAConfig
-from fla.models.utils import Cache, FLAGenerationMixin
+from fla.models.utils import Cache, FLAGenerationMixin, prepare_causal_lm_labels
 from fla.modules import FusedCrossEntropyLoss, FusedLinearCrossEntropyLoss, RMSNorm
 from fla.modules import GatedMLP as PrecondKDAMLP
 from fla.modules.l2warp import l2_warp
@@ -387,7 +387,7 @@ class PrecondKDAForCausalLM(PrecondKDAPreTrainedModel, FLAGenerationMixin):
             else:
                 criterion = self.criterion
             labels = labels.to(hidden_states.device)
-            labels = torch.cat((labels[..., 1:], torch.full_like(labels[:, :1], criterion.ignore_index)), 1)
+            labels = prepare_causal_lm_labels(labels, criterion.ignore_index, kwargs.get("cu_seqlens"))
             if fuse_linear_and_cross_entropy:
                 loss = criterion(hidden_states, labels, self.lm_head.weight, self.lm_head.bias)
             else:
